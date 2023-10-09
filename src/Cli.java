d946b42a9f8756b49bbc97cc24da04e56f6f759b
yuvalCat2006 <kats.yuval23@gamil.com>
2023-10-09 19:48:55 +0300

    added base cli

---
+++ src/Cli.java
@@ -0,0 +1,11 @@
+public class Cli {
+    Guess[] guesses;
+    Keyboard keyboard;
+
+    public Cli(Guess[] guesses, Keyboard keyboard){
+        this.guesses = guesses;
+        this.keyboard = keyboard;
+        
+    }
+
+}
