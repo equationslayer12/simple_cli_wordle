6b1f3fa2afebd6f92afdf7f2376a49fab6001f8f
equationslayer12 <roy0708usa@gmail.com>
2023-10-10 21:56:27 +0300

    wordle gameplay (no cli yet)

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -1,11 +1,18 @@
 public class Keyboard {
     Letter[] letters;
-    String alphabet = "qwertyuiopasdfghjklzxcvbnm";
+    String alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
     public Keyboard() {
         this.letters = new Letter[alphabet.length()];
 
         for (int i = 0; i < alphabet.length(); i++) {
             letters[i] = new Letter(alphabet.charAt(i), LetterStatus.unchecked);
+        }
+    }
+
+    public void updateLetter(char letter, LetterStatus letterStatus) {
+        for (Letter keyboardLetter : this.letters) {
+            if (keyboardLetter.letter == letter)
+                keyboardLetter.status = letterStatus;
         }
     }
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,3 +1,67 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Wordle {
-    Keyboard keyboard = new Keyboard();
+    Keyboard keyboard;
+    Guess[] guesses;
+
+    static int numberOfGuesses = 6;
+
+    public void play() {
+        String targetWord = chooseRandomWord();
+        
+        this.keyboard = new Keyboard();
+        this.guesses = new Guess[numberOfGuesses];
+
+        Cli cli = new Cli(guesses, keyboard);
+        Scanner scan = new Scanner(System.in);
+
+        for (int guessIndex = 0; guessIndex < numberOfGuesses; guessIndex++) {
+            cli.render();
+            String input = "";
+            while (input.length() != 5) {
+                input = scan.nextLine().toUpperCase();
+            }
+            Guess guess = new Guess(new Letter[input.length()]);
+            for (int letterIndex = 0; letterIndex < input.length(); letterIndex++) {
+                char letter = input.charAt(letterIndex);
+                LetterStatus letterStatus = getLetterStatus(letter, targetWord);
+                guess.letters[letterIndex] = new Letter(letter, letterStatus);
+                keyboard.updateLetter(letter, letterStatus);
+            }
+        }
+    }
+
+    private LetterStatus getLetterStatus(char letter, String targetWord) {
+        if (letterIsWrong(letter, targetWord))
+            return LetterStatus.wrong;
+
+        else if (letterIsCorrect(letter, targetWord))
+            return LetterStatus.correct;
+
+        else
+            return LetterStatus.wrongPosition;
+
+    }
+
+    private boolean letterIsWrong(char letter, String targetWord) {
+        for (int letterIndex = 0; letterIndex < targetWord.length(); letterIndex++) {
+            if (letter == targetWord.charAt(letterIndex))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean letterIsCorrect(char letter, String targetWord) {
+        for (int letterIndex = 0; letterIndex < targetWord.length(); letterIndex++) {
+            if (letter == targetWord.charAt(letterIndex))
+                return true;
+        }
+        return false;
+    }
+
+
+    private String chooseRandomWord() {
+        return "SONIC";
+    }
 }
